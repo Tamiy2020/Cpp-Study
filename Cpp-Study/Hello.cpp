8bf3f8da09848625b481d1a863d69f218a394714
Tamiy <1145849201@qq.com>
2020-06-06 11:24:39 +0800

    注释学习

--- Cpp-Study/Hello.cpp
+++ Cpp-Study/Hello.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
 using namespace std;
 
+/*
+    main是一个程序的入口
+	每个程序都必须有且只有一个这个函数
+*/
 int main()
 {
-	cout << "hello world" << endl;
+	cout << "hello world" << endl; //在控制台中输出hello world
 	system("pause");
 	return 0;
 }
